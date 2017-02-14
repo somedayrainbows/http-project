b48d91a49522176c081b1d8ecfa66d3dc09a707e
somedayrainbows <ebassity@gmail.com>
2017-02-14 14:30:28 -0700

    iteration 0 complete

--- my_server_0.rb
+++ my_server_0.rb
@@ -7,42 +7,50 @@ require 'pry'
 require 'socket'
 
 class HTTP_yeah
-  attr_reader :tcp_server, :counter
+  attr_reader :tcp_server
+  attr_accessor :counter
 
   def initialize
     @tcp_server = TCPServer.new(9292)
-    @counter = 1
+    @counter = 0
   end
 
   def handle_connection
     while client = @tcp_server.accept
-      puts "<h1>" + "Hello, World! (#{@counter})\n" + "</h1>"
-      counter += 1
+
+      puts "Ready for a request"
+      request_lines = []
+      while line = client.gets and !line.chomp.empty?
+        request_lines << line.chomp
+      end
+
+      puts "Got this request:"
+      puts request_lines.inspect
+
+      puts "Sending response."
+        if request_lines[0] == "GET /favicon.ico HTTP/1.1"
+          client.puts["http/1.1 404 not-found"]
+          next
+        end
+      response = "<pre>" + "Hello, World! (#{counter})" + "</pre>"
+      output = "<html><head></head><body>#{response}</body></html>"
+      headers = ["http/1.1 200 ok",
+                "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+                "server: ruby",
+                "content-type: text/html; charset=iso-8859-1",
+                "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+      client.puts headers
+      client.puts output
+
+      counter+=
+
+      puts ["Wrote this response:", headers, output].join("\n")
+      client.close
+      puts "\nResponse complete, exiting."
     end
-
-  puts "Ready for a request"
-  request_lines = []
-  while line = client.gets and !line.chomp.empty?
-    request_lines << line.chomp
-  end
-
-  puts "Got this request:"
-  puts request_lines.inspect
-
-  puts "Sending response."
-  response = "<pre>" + request_lines.join("\n") + "</pre>"
-  output = "<html><head></head><body>#{response}</body></html>"
-  headers = ["http/1.1 200 ok",
-            "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
-            "server: ruby",
-            "content-type: text/html; charset=iso-8859-1",
-            "content-length: #{output.length}\r\n\r\n"].join("\r\n")
-  client.puts headers
-  client.puts output
-
-  puts ["Wrote this response:", headers, output].join("\n")
-  client.close
-  puts "\nResponse complete, exiting."
 
   end
 end
+
+http = HTTP_yeah.new
+http.handle_connection
--- time.rb
+++ time.rb
@@ -1,11 +1,20 @@
 require 'socket'
 
-server = TCPServer.new 9292
+server = TCPServer.new('localhost', 9292)
+
+counter = 0
 
 loop do
-  Thread.start(server.accept) do |client|
-    client.puts "Hello !"
-    client.puts "Time is #{Time.now}"
-    client.close
-  end
+  socket = server.accept
+  request = socket.gets
+  STDERR.puts request
+  response = "Hello World! (#{counter})\n"
+  socket.print "HTTP/1.1 200 OK\r\n" +
+               "Content-Type: text/plain\r\n" +
+               "Content-Length: #{response.bytesize}\r\n" +
+               "Connection: close\r\n"
+  socket.print "\r\n"
+  socket.print response
+  socket.close
+  counter += 1
 end
